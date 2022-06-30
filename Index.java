1ebfe2560bdd7e7d823e964ea6bcbadf984f53dd
ArmenSS <98808495+ArmenSS@users.noreply.github.com>
2022-06-30 10:55:26 +0400

    Update Index.java

--- Index.java
+++ Index.java
@@ -3,12 +3,16 @@ package ru.surf;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+// не принята использовать импорт такого рода
+//pri  импортe с *-ом придет вес покет
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 
 public class Index {
+    
+    //поля надо сделать private
     TreeMap<String, List<Pointer>> invertedIndex;
 
     ExecutorService pool;
@@ -18,6 +22,8 @@ public class Index {
         invertedIndex = new TreeMap<>();
     }
 
+
+    // принимающие аргументы должны быть final
     public void indexAllTxtInPath(String pathToDir) throws IOException {
         Path of = Path.of(pathToDir);
 
@@ -31,11 +37,11 @@ public class Index {
         pool.submit(new IndexTask(files));
         pool.submit(new IndexTask(files));
     }
-
+// будет лучше использовать SortedMap
     public TreeMap<String, List<Pointer>> getInvertedIndex() {
         return invertedIndex;
     }
-
+// имя метода нужно начинать маленьким буквом 
     public List<Pointer> GetRelevantDocuments(String term) {
         return invertedIndex.get(term);
     }
@@ -45,7 +51,11 @@ public class Index {
     }
 
     static class Pointer {
+// нужно getter setter
+     
         private Integer count;
+        
+// поле filePath может быть final
         private String filePath;
 
         public Pointer(Integer count, String filePath) {
@@ -72,18 +82,20 @@ public class Index {
             try {
                 Path take = queue.take();
                 List<String> strings = Files.readAllLines(take);
-
+                
+// можно также использовать parallelStream() если в файле будет много строков,поскольку здесь используется ExecutorService 
                 strings.stream().flatMap(str -> Stream.of(str.split(" "))).forEach(word -> invertedIndex.compute(word, (k, v) -> {
                     if (v == null) return List.of(new Pointer(1, take.toString()));
                     else {
                         ArrayList<Pointer> pointers = new ArrayList<>();
-
+// будет лучше использовать equalsIgnoreCase
                         if (v.stream().noneMatch(pointer -> pointer.filePath.equals(take.toString()))) {
                             pointers.add(new Pointer(1, take.toString()));
                         }
 
                         v.forEach(pointer -> {
                             if (pointer.filePath.equals(take.toString())) {
+// можно pointer.count++;
                                 pointer.count = pointer.count + 1;
                             }
                         });
@@ -94,8 +106,9 @@ public class Index {
                     }
 
                 }));
-
+// будет правильно InterruptedException и IOException поставить в try catch отдельно
             } catch (InterruptedException | IOException e) {
+// в RuntimeException() писать e.message                  
                 throw new RuntimeException();
             }
         }
